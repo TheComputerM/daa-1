5606540beeb51610db472c50f30198caf22c9dbf
TheComputerM <mudit.somani00@gmail.com>
2025-03-23 23:11:24 +0530

    feat: deploy to gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// @ts-ignore
 import '@fontsource-variable/outfit';
 import './index.css'
 import App from './App.tsx'
